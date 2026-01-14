7d867220a5451b51b0024b2a88cde8a0847a629b
Hiroshi Urayama <asj.hiroshi@gmail.com>
2026-01-14 10:30:26 +0900

    ダミーコミット

--- ps/js-debug/04-typo-error.js
+++ ps/js-debug/04-typo-error.js
@@ -1,5 +1,5 @@
 const button = document.querySelector("button");
-// 間違い: addeventlistener (すべて小文字)
+// 間違い: addeventlistener (すべて小文字) 
 button.addeventlistener("click", showMessage);
 
 function showMessage() {
